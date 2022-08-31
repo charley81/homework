547618b5267246ad05328a796b705b24ee46da34
ChrisKnowsDev <chrisharley81@gmail.com>
2022-08-31 19:36:27 -0400

    main functionality complete

--- lightBulb/app.js
+++ lightBulb/app.js
@@ -2,17 +2,19 @@ const switchBtn = document.querySelector('.btn-switch')
 const lightbulb = document.querySelector('.lightbulb')
 const title = document.querySelector('h1')
 
-const switchLight = e => {
-  if (e.target.classList.value.split(' ').includes('on')) {
-    e.target.classList.remove('on')
-    document.body.style.backgroundColor = 'red'
-    lightbulb.src = lightbulb.src =
-      'http://127.0.0.1:5500/lightBulb/images/light-off.png'
+let showLight = false
+switchBtn.addEventListener('click', () => {
+  if (showLight) {
+    lightbulb.classList.remove('active')
+    showLight = false
+    lightbulb.src = 'http://127.0.0.1:5500/lightBulb/images/light-off.png'
+    document.body.style.backgroundColor = 'var(--color-light)'
+    title.style.color = 'var(--color-dark)'
   } else {
-    switchBtn.classList.add('on')
-    lightbulb.src = lightbulb.src =
-      'http://127.0.0.1:5500/lightBulb/images/light-on.png'
+    showLight = true
+    lightbulb.classList.add('active')
+    lightbulb.src = 'http://127.0.0.1:5500/lightBulb/images/light-on.png'
+    document.body.style.backgroundColor = 'var(--color-dark)'
+    title.style.color = 'var(--color-light)'
   }
-}
-
-switchBtn.addEventListener('click', switchLight)
+})
