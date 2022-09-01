6e0094627fa804fd004c43c1137c0983c93e0a2b
ChrisKnowsDev <chrisharley81@gmail.com>
2022-09-01 17:41:35 -0400

    added title animation

--- lightBulb/app.js
+++ lightBulb/app.js
@@ -7,14 +7,12 @@ let showLight = false
 switchBtn.addEventListener('click', () => {
   audio.play()
   if (showLight) {
-    lightbulb.classList.remove('active')
     showLight = false
     lightbulb.src = 'http://127.0.0.1:5500/lightBulb/images/light-off.png'
     document.body.style.backgroundColor = 'var(--color-light)'
     title.style.color = 'var(--color-dark)'
   } else {
     showLight = true
-    lightbulb.classList.add('active')
     lightbulb.src = 'http://127.0.0.1:5500/lightBulb/images/light-on.png'
     document.body.style.backgroundColor = 'var(--color-dark)'
     title.style.color = 'var(--color-light)'
