605fb86094c4eaf3bdcfa95df5baa5d8a1408d14
ChrisKnowsDev <chrisharley81@gmail.com>
2022-09-29 16:29:03 -0400

    index view styled

--- captains-log/models/log-model.js
+++ captains-log/models/log-model.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose')
 
-const logSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const logSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    entry: {
+      type: String,
+      required: true,
+    },
+    broken: {
+      type: Boolean,
+      default: true,
+    },
   },
-  entry: {
-    type: String,
-    required: true,
-  },
-  broken: {
-    type: Boolean,
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 module.exports = mongoose.model('Log', logSchema)
--- captains-log/views/logs/index.js
+++ captains-log/views/logs/index.js
@@ -8,15 +8,27 @@ class Index extends React.Component {
     return (
       <Layout title="all logs" group="logs">
         <div className="wrapper">
-          <h1>All Logs</h1>
+          <div className="header">
+            <h1>All Logs</h1>
+            <a href="/logs/new">create new</a>
+          </div>
+
           <div className="items">
             {logs.map(log => {
-              const { title, entry, broken } = log
+              const { title, entry, broken, _id } = log
 
               return (
-                <ul>
-                  <li>{title}</li>
-                  <li>{entry}</li>
+                <ul key={_id} className="item">
+                  <li>
+                    <span className="bold">Title: </span> {title}
+                  </li>
+                  <li>
+                    <span className="bold">Entry: </span> {entry}
+                  </li>
+                  <li>
+                    <span className="bold">Broken: </span>
+                    {broken ? 'Yes' : 'No'}
+                  </li>
                 </ul>
               )
             })}
